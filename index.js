675a4a311e768c0b6123ea91a22b1f0109c5d6de
Alextxnk <alexsolov766@gmail.com>
2022-09-27 11:12:48 +0300

    first commit

--- index.js
+++ index.js
@@ -3,47 +3,15 @@
 
 const canvasPlot = document.getElementById('canvas_plot');
 const ctx = canvasPlot.getContext('2d'); // определяем контекст, чтобы получить доступ к элементам рисования 
-// в canvas бывает два вида контекста 2d и 3d
-
-
-/* // квадраты
-ctx.fillRect(0, 0, 100, 100); // (x, y, xSize, ySize)
-ctx.fillStyle = '#0000ff'; // синий
-ctx.fillRect(100, 100, 200, 200);
-
-
-// линии
-// линия из (300, 300) в (400, 400)
-ctx.beginPath(); // начинаем рисование
-ctx.moveTo(300, 300);
-ctx.lineTo(400, 400);
-ctx.stroke(); // рисует саму линию
-ctx.closePath(); // заканчиваем рисование
-
-ctx.beginPath();
-ctx.strokeStyle = '#ff0000';
-ctx.moveTo(320, 300);
-ctx.lineTo(400, 400);
-ctx.stroke();
-ctx.closePath();
-
-
-// текст
-ctx.font = '30px Arial';
-ctx.textAlign = 'left'; // right, горизонталь
-ctx.textBaseline = 'top'; // bottom, вертикаль 
-ctx.fillText('Test', 300, 400);
-ctx.fillRect(300, 400, 2, 2); */
 
 
 // рисуем сетку
 const canvasPlotWidth = canvasPlot.clientWidth;
 const canvasPlotHeigth = canvasPlot.clientHeight;
-console.log(canvasPlotWidth, canvasPlotHeigth);
 
-const scaleX = 50;
-const scaleY = 50;
-const shiftNumberNames = 5; // отступы для чисел на оси
+const scaleX = 25;
+const scaleY = 25;
+const shiftNumberNames = scaleX / 10; // отступы для чисел на оси
 const shiftAxisNames = scaleX / 2.5;
 const shiftxAxisNames = scaleX / 1.6;
 
