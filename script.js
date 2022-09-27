f669092f096ef403adc7cf385abd422ac5024cfa
Alextxnk <alexsolov766@gmail.com>
2022-09-27 13:42:59 +0300

    first commit

--- script.js
+++ script.js
@@ -3,6 +3,36 @@
 
 const canvasPlot = document.getElementById('canvas_plot');
 const ctx = canvasPlot.getContext('2d'); // определяем контекст, чтобы получить доступ к элементам рисования 
+// в canvas бывает два вида контекста 2d и 3d
+
+/* // квадраты
+ctx.fillRect(0, 0, 100, 100); // (x, y, xSize, ySize)
+ctx.fillStyle = '#0000ff'; // синий
+ctx.fillRect(100, 100, 200, 200);
+
+
+// линии
+// линия из (300, 300) в (400, 400)
+ctx.beginPath(); // начинаем рисование
+ctx.moveTo(300, 300);
+ctx.lineTo(400, 400);
+ctx.stroke(); // рисует саму линию
+ctx.closePath(); // заканчиваем рисование
+
+ctx.beginPath();
+ctx.strokeStyle = '#ff0000';
+ctx.moveTo(320, 300);
+ctx.lineTo(400, 400);
+ctx.stroke();
+ctx.closePath();
+
+
+// текст
+ctx.font = '30px Arial';
+ctx.textAlign = 'left'; // right, горизонталь
+ctx.textBaseline = 'top'; // bottom, вертикаль 
+ctx.fillText('Test', 300, 400);
+ctx.fillRect(300, 400, 2, 2); */
 
 
 // рисуем сетку
@@ -16,7 +46,7 @@ const shiftAxisNames = scaleX / 2.5;
 const shiftxAxisNames = scaleX / 1.6;
 
 const xAxis = Math.round(canvasPlotWidth / scaleX / 2) * scaleX;
-const yAxis = Math.round(canvasPlotHeigth / scaleY / 2) * scaleY;
+const yAxis = (canvasPlotHeigth / scaleY / 2) * scaleY;
 
 ctx.font = `${Math.round(scaleX / 2)}px Arial`;
 ctx.textAlign = 'left';
@@ -58,43 +88,15 @@ ctx.moveTo(0, yAxis);
 ctx.lineTo(canvasPlotWidth, yAxis);
 ctx.fillText('x', canvasPlotWidth - shiftAxisNames, yAxis - shiftxAxisNames);
 
-ctx.stroke();
-ctx.closePath();
+ctx.stroke(); 
+ctx.closePath(); 
 
 
 // рисуем график
 // y = x^2
-/* ctx.fillStyle = '#ff0000';
+ctx.fillStyle = '#ff0000';
 for (let i = 0; i <= canvasPlotWidth; i = i + 0.5) {
    const x = (i - xAxis) / scaleX;
    const y = Math.pow(x, 2);
    ctx.fillRect(x * scaleX + xAxis, yAxis - scaleY * y, 4, 4);
-} */
-
-
-
-const n = 100; // количество шагов
-const h = 0.15; // длина шага
-const y0 = 41; // начальное значение y
-
-let appData = {
-   resObj: {}
-};
-
-
-// аналитическое
-appData.resObj[0] = y0;
-
-for (let i = 1; i < 200; i++) {
-   let t = i * h;
-   appData.resObj[i] = 1 / (1 / 3 * Math.atan(t / 3) + 1 / 41);
-}
-console.dir(appData.resObj);
-
-ctx.fillStyle = '#000000';
-for (let key in appData.resObj) {
-   console.log('key:', key, 'resObj[key]:', appData.resObj[key]);
-   let x = (key - xAxis) / scaleX;
-   let y = appData.resObj[key];
-   ctx.fillRect(x * scaleX + xAxis * 2, yAxis - scaleY * y, 2, 2);
 }
