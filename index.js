e2ce614f26018d814963a0b897bc79857293cbe7
Alextxnk <alexsolov766@gmail.com>
2022-09-27 17:21:40 +0300

    first commit

--- index.js
+++ index.js
@@ -4,13 +4,12 @@
 const canvasPlot = document.getElementById('canvas_plot');
 const ctx = canvasPlot.getContext('2d'); // определяем контекст, чтобы получить доступ к элементам рисования 
 
-
 // рисуем сетку
 const canvasPlotWidth = canvasPlot.clientWidth;
 const canvasPlotHeigth = canvasPlot.clientHeight;
 
-const scaleX = 25;
-const scaleY = 25;
+const scaleX = 50;
+const scaleY = 50;
 const shiftNumberNames = scaleX / 10; // отступы для чисел на оси
 const shiftAxisNames = scaleX / 2.5;
 const shiftxAxisNames = scaleX / 1.6;
@@ -19,7 +18,8 @@ const xAxis = Math.round(canvasPlotWidth / scaleX / 2) * scaleX;
 const yAxis = Math.round(canvasPlotHeigth / scaleY / 2) * scaleY;
 
 ctx.font = `${Math.round(scaleX / 2)}px Arial`;
-ctx.textAlign = 'left';
+
+ctx.textAlign = 'right';
 ctx.textBaseline = 'top';
 
 // вертикальные линии
@@ -30,7 +30,7 @@ for (let i = 0; i <= canvasPlotWidth; i = i + scaleX) {
    ctx.moveTo(i, 0);
    ctx.lineTo(i, canvasPlotHeigth);
 
-   ctx.fillText((i - xAxis) / scaleX, i + shiftNumberNames, yAxis + shiftNumberNames);
+   ctx.fillText((i - scaleX) / scaleX, i - shiftNumberNames, canvasPlotHeigth - scaleY + shiftNumberNames);
 }
 
 // горизонтальные линии
@@ -38,39 +38,27 @@ for (let i = 0; i <= canvasPlotHeigth; i = i + scaleY) {
    ctx.moveTo(0, i);
    ctx.lineTo(canvasPlotWidth, i);
 
-   ctx.fillText((yAxis - i) / scaleY, xAxis + shiftNumberNames, i + shiftNumberNames);
+   ctx.fillText((canvasPlotHeigth - scaleY - i) / scaleY, scaleX - shiftNumberNames, i + shiftNumberNames);
 }
 
 ctx.stroke(); // рисует саму линию
 ctx.closePath(); // заканчиваем рисование
 
 
-
 // рисуем главные оси 
 ctx.beginPath();
 ctx.strokeStyle = '#000000';
 
-ctx.moveTo(xAxis, 0);
-ctx.lineTo(xAxis, canvasPlotHeigth);
-ctx.fillText('y', xAxis - shiftAxisNames, 0);
+ctx.moveTo(scaleX, 0);
+ctx.lineTo(scaleX, canvasPlotHeigth);
+ctx.fillText('y', scaleX + shiftAxisNames, 0);
 
-ctx.moveTo(0, yAxis);
-ctx.lineTo(canvasPlotWidth, yAxis);
-ctx.fillText('x', canvasPlotWidth - shiftAxisNames, yAxis - shiftxAxisNames);
+ctx.moveTo(0, canvasPlotHeigth - scaleY);
+ctx.lineTo(canvasPlotWidth, canvasPlotHeigth - scaleY);
+ctx.fillText('x', canvasPlotWidth - shiftAxisNames, canvasPlotHeigth - scaleY - shiftxAxisNames);
 
 ctx.stroke();
 ctx.closePath();
-
-
-// рисуем график
-// y = x^2
-/* ctx.fillStyle = '#ff0000';
-for (let i = 0; i <= canvasPlotWidth; i = i + 0.5) {
-   const x = (i - xAxis) / scaleX;
-   const y = Math.pow(x, 2);
-   ctx.fillRect(x * scaleX + xAxis, yAxis - scaleY * y, 4, 4);
-} */
-
 
 
 const n = 100; // количество шагов
@@ -78,23 +66,114 @@ const h = 0.15; // длина шага
 const y0 = 41; // начальное значение y
 
 let appData = {
-   resObj: {}
+   resObj: {},
+   explicitScheme: {},
+   implicitScheme: {},
+   weightsScheme: {}
 };
 
+function first(t, y) {
+   return -(Math.pow(y, 2) / (9 + Math.pow(t, 2)));
+}
 
-// аналитическое
+
+// аналитическое -------------
 appData.resObj[0] = y0;
 
-for (let i = 1; i < 200; i++) {
+for (let i = 1; i < n; i++) {
    let t = i * h;
    appData.resObj[i] = 1 / (1 / 3 * Math.atan(t / 3) + 1 / 41);
 }
-console.dir(appData.resObj);
 
+// рисуем график
 ctx.fillStyle = '#000000';
 for (let key in appData.resObj) {
-   console.log('key:', key, 'resObj[key]:', appData.resObj[key]);
    let x = (key - xAxis) / scaleX;
    let y = appData.resObj[key];
-   ctx.fillRect(x * scaleX + xAxis * 2, yAxis - scaleY * y, 2, 2);
+   ctx.fillRect(x * scaleX + xAxis + scaleX, canvasPlotHeigth - scaleY - scaleY * y, 2, 2);
 }
+
+
+// Явная схема ---------------
+appData.explicitScheme[0] = y0;
+
+for (let i = 0; i < n - 1; i++) {
+   let t = i * h;
+   let y = appData.explicitScheme[i];
+   appData.explicitScheme[i + 1] = y + h * first(t, y);
+}
+
+// рисуем график
+ctx.fillStyle = '#ff0000';
+for (let key in appData.explicitScheme) {
+   let x = (key - xAxis) / scaleX;
+   let y = appData.explicitScheme[key];
+   ctx.fillRect(x * scaleX + xAxis + scaleX, canvasPlotHeigth - scaleY - scaleY * y, 2, 2);
+}
+
+
+// Неявная схема -------------
+appData.implicitScheme[0] = y0;
+
+for (let i = 0; i < n - 1; i++) {
+   let t = i * h;
+   let y = appData.implicitScheme[i];
+   let tn = 9 + Math.pow(t + h, 2);
+   appData.implicitScheme[i + 1] = -tn / (2 * h) + Math.sqrt(Math.pow(tn / (2 * h), 2) + y * tn / h);
+}
+
+// рисуем график
+ctx.fillStyle = '#00ff00';
+for (let key in appData.implicitScheme) {
+   let x = (key - xAxis) / scaleX;
+   let y = appData.implicitScheme[key];
+   ctx.fillRect(x * scaleX + xAxis + scaleX, canvasPlotHeigth - scaleY - scaleY * y, 2, 2);
+}
+
+
+// Схема с весами ------------
+appData.weightsScheme[0] = y0;
+
+for (let i = 0; i < n - 1; i++) {
+   let t = i * h;
+   let y = appData.weightsScheme[i];
+   appData.weightsScheme[i + 1] = y + h * first(t + h / 2, y + h / 2 * first(t, y));
+}
+
+// рисуем график
+ctx.fillStyle = '#0000ff';
+for (let key in appData.weightsScheme) {
+   let x = (key - xAxis) / scaleX;
+   let y = appData.weightsScheme[key];
+   ctx.fillRect(x * scaleX + xAxis + scaleX, canvasPlotHeigth - scaleY - scaleY * y, 2, 2);
+}
+
+
+// погрешность
+let deviation = 0;
+let resObjArr = []; // аналитическое
+let explicitSchemeArr = []; // Явная схема
+
+// аналитическое
+for (let key in appData.resObj) {
+   if (appData.resObj.hasOwnProperty(key)) {
+      resObjArr.push(appData.resObj[key]);
+   }
+}
+console.dir(resObjArr);
+
+
+// погрешность явной схемы
+for (let i = 0; i < n; i++) {
+   // deviation += Math.abs();
+}
+
+/* double pogr = 0;
+double[] orig = Original(); // аналитическое
+double[] mas = First(); // Явная схема
+for (int i = 0; i < N; i++) {
+   pogr += Math.Abs(orig[i] - mas[i]) / orig[i];
+}
+pogr /= N;
+pogr *= 100;
+pogr = Math.Round(pogr,1); */
